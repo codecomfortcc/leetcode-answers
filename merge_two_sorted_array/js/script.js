77b0f6799aa48a5e274ec80124c5d1e8256f4078
yasovardhan masani <yasovardhanmasani@gmail.com>
2024-04-07 22:28:36 +0530

    merge two sorted array

---
+++ merge_two_sorted_array/js/script.js
@@ -0,0 +1,14 @@
+// merge two sorted array using recursion
+const mergeTwoLists = function(list1, list2) {
+  if (!list1) return list2;
+    if (!list2) return list1;
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+  
+};
+console.log(mergeTwoLists([1,2,4],[1,3,5]))
